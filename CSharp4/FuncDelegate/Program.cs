d2979fa8b6fb50055f4a7e441247b9498569d9aa
RendaniTshinakaho <r.nkhumeleni@gmail.com>
2017-02-26 09:58:26 +0200

    Func Delegate

---
+++ CSharp4/FuncDelegate/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FuncDelegate
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, int> totalLength = (s2) => s2.Length;
+            int total = totalLength("Hello");
+            Console.WriteLine(total);
+        }
+    }
+}
