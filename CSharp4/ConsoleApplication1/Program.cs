a347d0d9f0c610fc31c1e47274c4c0c0da480241
RendaniTshinakaho <r.nkhumeleni@gmail.com>
2017-02-26 11:49:54 +0200

    Added more stuff

---
+++ CSharp4/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Hello";
+            var results = ReverseString(word);
+            Console.WriteLine(results);
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] arrayChar = s.ToCharArray();
+            Array.Reverse(arrayChar);
+            return new string(arrayChar);
+        }
+    }
+}
---
+++ CSharp4/ConsoleApplication2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string word = string.Concat("Hello ", "World!".ToUpper());
+            Console.WriteLine(word);
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
