184c73a494a9c790b60b7b6e7f6dd1c250d7d333
RendaniTshinakaho <r.nkhumeleni@gmail.com>
2017-02-26 10:30:27 +0200

    Exception Handling Test

---
+++ CSharp4/ExceptionHandler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Test.Display(null);
+            }
+            catch (ArgumentNullException exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+
+   public class Test
+    {
+       public static void Display(string name)
+        {
+            if (name == null) throw new ArgumentNullException("name");
+            Console.WriteLine();  
+        }
+    }
+}
---
+++ CSharp4/ExtensionMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = StringHandler.IsCapitalized("rendani");
+            Console.WriteLine(results); 
+        }
+    }
+
+    public static class StringHandler
+    {
+        public static bool IsCapitalized(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return false;
+            }
+            return char.IsUpper(s[0]);
+        }
+    }
+}
