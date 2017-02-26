efbb45e6bb6f5fa2cdf0668a859c10242b6646c9
RendaniTshinakaho <r.nkhumeleni@gmail.com>
2017-02-26 09:27:08 +0200

    Added delegate function

--- CSharp4/OptionalParameters/Program.cs
+++ CSharp4/OptionalParameters/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace OptionalParameters
 {
+    internal delegate int Transformer(int x);
     class Program
     {
+    
         static void Main(string[] args)
         {
+            Transformer t = Square;
+            int result = t(3);
+            Console.WriteLine(result);
+        }
+
+        private static int Square(int x)
+        {
+            return x*x;
         }
     }
 }
